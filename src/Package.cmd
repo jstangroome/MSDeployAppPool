0f5251fcc10f0be921b0c8cdd7d3df8fc55b52c0
Jason Stangroome <jason@codeassassin.com>
2012-05-29 10:06:40 +1000

    More

--- src/Package.cmd
+++ src/Package.cmd
@@ -1,8 +1,10 @@
 set msdeployexe=C:\Program Files (x86)\IIS\Microsoft Web Deploy V2\msdeploy.exe
 
 rmdir /s /q "%~dp0output"
+if errorlevel 1 exit /b 1
 mkdir "%~dp0output"
 
+rem call "%msdeployexe%" -verb:sync -source:appPoolConfig=TemplateApplicationPool -dest:archiveDir="%~dp0output\archiveDir" -declareParamFile="%~dp0Package.declareParam.xml"
 call "%msdeployexe%" -verb:sync -source:appPoolConfig=TemplateApplicationPool -dest:package="%~dp0output\TemplateApplicationPool.zip" -declareParamFile="%~dp0Package.declareParam.xml"
 
 icacls "%~dp0output" /reset /t
--- src/Test.cmd
+++ src/Test.cmd
@@ -1,5 +1,10 @@
 set msdeployexe=C:\Program Files (x86)\IIS\Microsoft Web Deploy V2\msdeploy.exe
 
 call "%msdeployexe%" -verb:getParameters -source:package="%~dp0output\TemplateApplicationPool.zip"
+rem call "%msdeployexe%" -verb:sync -source:package="%~dp0output\TemplateApplicationPool.zip" -dest:archiveDir="%~dp0output\archiveDir" -setParam:managedPipelineMode=Classic -verbose
 
-call "%msdeployexe%" -verb:sync -source:package="%~dp0output\TemplateApplicationPool.zip" -dest:auto -setParamFile="%~dp0Test.SetParam.xml"
+call "%msdeployexe%" -verb:sync -source:package="%~dp0output\TemplateApplicationPool.zip" -dest:auto -setParamFile="%~dp0Test.SetParam.xml" -verbose
+rem call "%msdeployexe%" -verb:sync -source:package="%~dp0output\TemplateApplicationPool.zip" -dest:auto -setParam:providerPath=TestApplicationPool -setParam:managedPipelineMode=Classic -verbose
+rem call "%msdeployexe%" -verb:sync -source:archiveDir="%~dp0output\archiveDir" -dest:auto -setParam:managedPipelineMode=Classic -verbose
+
+rem icacls "%~dp0output" /reset /t
